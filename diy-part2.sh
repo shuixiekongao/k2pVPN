ba359701d8386f0406a2681050aeea4a8934ac2b
sunrain <78016487+shuixiekongao@users.noreply.github.com>
2026-01-11 22:29:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认管理 IP (可选，如果你想改成 192.168.2.1)
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 强制锁定 K2P 硬件架构（多重保险）
+echo "CONFIG_TARGET_ramips=y" >> .config
+echo "CONFIG_TARGET_ramips_mt7621=y" >> .config
+echo "CONFIG_TARGET_ramips_mt7621_DEVICE_phicomm_k2p=y" >> .config
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 开启 Sing-box 唯一核心方案（方案 B）
+# 强制选中 Sing-box 及其 Luci 支持
+echo "CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Sing_Box=y" >> .config
+echo "CONFIG_PACKAGE_sing-box=y" >> .config
+
+# 强制剔除 Xray/V2ray 等占用空间巨大的核心
+sed -i 's/CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Core=y/# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Core is not set/' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Core=y/# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Core is not set/' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geodata=y/# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geodata is not set/' .config
+
+# 4. 极致瘦身策略（关键）
+# 开启二进制 UPX 压缩，这是 Sing-box 能进 K2P 的唯一机会
+echo "CONFIG_SING_BOX_COMPRESS_UPX=y" >> .config
+echo "CONFIG_USE_SSTRIP=y" >> .config
+
+# 彻底移除 K2P 用不到的组件（Docker、打印服务器、USB 驱动等）
+sed -i 's/CONFIG_PACKAGE_luci-app-docker=y/# CONFIG_PACKAGE_luci-app-docker is not set/' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-usb-printer=y/# CONFIG_PACKAGE_luci-app-usb-printer is not set/' .config
+sed -i 's/CONFIG_PACKAGE_luci-app-samba=y/# CONFIG_PACKAGE_luci-app-samba is not set/' .config
+
+# 5. 修改无线名称为 PDCN (初始生效)
+sed -i 's/OpenWrt/PDCN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
